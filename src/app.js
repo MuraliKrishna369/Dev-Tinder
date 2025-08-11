6b86ff8e3c6157639f3800b775111064420747c6
MuraliKrishna369 <muralikrishnapogiri369@gmail.com>
2025-08-11 07:42:21 +0530

    Created Database Model

--- src/app.js
+++ src/app.js
@@ -1,10 +1,35 @@
 const express = require("express");
-const {connectDB}  = require("./utils/database");
+const connectDB  = require("./config/database");
+const User = require("./models/user");
 const app = express()
+
+app.get("/user", (req, res) => {
+    res.send("Hello World!")
+})
+
+app.post("/signup", async (req, res) => {
+    const userInfo = {
+        firstName: "Murali",
+        lastName: "Krishna",
+        emailId: "murali@gmail.com",
+        password: "murali@1234",
+        gender: "male"
+    }
+    const user = new User(userInfo)
+    try{
+        await user.save()
+        res.send("User created")
+    }
+    catch(err){
+        res.send("User creation failed ", err)
+    }
+   
+    
+})
 
 connectDB()
 .then(() => {
     console.log("Database connected")
     app.listen(7777, () => console.log("server is running on port 7777"))
 })
-.catch((e) => console.log(e, "Database connection is failed"))
+.catch((err) => console.log(err, "Database connection is failed"))
--- src/utils/database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose")
-const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://muralikrishnapogiri369:z8CYtnhrJBpTeIaG@namastenode.zboglvd.mongodb.net/")
-}
-
-module.exports = {connectDB}
