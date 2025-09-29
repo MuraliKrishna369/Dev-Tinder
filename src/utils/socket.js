583fa18bd2038ed6e7946d38c6d6ed28dce0c66e
MuraliKrishna369 <muralikrishnapogiri369@gmail.com>
2025-09-29 16:48:40 +0530

    cors origin changed

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -7,7 +7,7 @@ const UserStatus = require("../models/status");
 intializeSocket = (server) => {
     const io = socket(server, {
         cors : {
-            origin: "http://localhost:5173"
+            origin: "*"
         }
     })
     io.on("connection", (socket) => {
